95b539baff601e58ce3ca99afc4c6bf1195df3e7
Erik Hellman <erik.hellman@hellsoft.se>
2018-01-14 16:03:01 +0100

    First commit

---
+++ app/src/main/java/se/hellsoft/kotlinasyncwithcoroutines/CoroutineAndroidLoader.kt
@@ -0,0 +1,60 @@
+package se.hellsoft.kotlinasyncwithcoroutines
+
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.OnLifecycleEvent
+import android.util.Log
+import kotlinx.coroutines.experimental.CoroutineStart
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.newFixedThreadPoolContext
+
+// Quick & dirty logcat extensions
+inline fun <reified T> T.logd(message: () -> String) = Log.d(T::class.simpleName, message())
+inline fun <reified T> T.loge(error: Throwable, message: () -> String) = Log.d(T::class.simpleName, message(), error)
+
+internal class CoroutineLifecycleListener(private val deferred: Deferred<*>) : LifecycleObserver {
+  @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+  fun cancelCoroutine() {
+    if (!deferred.isCancelled) {
+      deferred.cancel()
+    }
+  }
+}
+
+// CoroutineContext running on background threads.
+internal val Background = newFixedThreadPoolContext(Runtime.getRuntime().availableProcessors() * 2, "Loader")
+
+/**
+ * Creates a lazily started coroutine that runs <code>loader()</code>.
+ * The coroutine is automatically cancelled using the CoroutineLifecycleListener.
+ */
+fun <T> LifecycleOwner.load(loader: suspend () -> T): Deferred<T> {
+  val deferred = async(context = Background, start = CoroutineStart.LAZY) {
+    loader()
+  }
+
+  lifecycle.addObserver(CoroutineLifecycleListener(deferred))
+  return deferred
+}
+
+/**
+ * Extension function on <code>Deferred<T><code> that creates a launches a coroutine which
+ * will call <code>await()</code> and pass the returned value to <code>block()</code>.
+ */
+infix fun <T> Deferred<T>.then(block: suspend (T) -> Unit): Job {
+  return launch(context = UI) {
+    try {
+      block(this@then.await())
+    } catch (e: Exception) {
+      // Just log the exception to confirm when we get cancelled (Expect JobCancellationException)
+      loge(e) { "Exception in then()!" }
+      throw e
+    }
+  }
+}
+
---
+++ app/src/main/java/se/hellsoft/kotlinasyncwithcoroutines/MainActivity.kt
@@ -0,0 +1,74 @@
+package se.hellsoft.kotlinasyncwithcoroutines
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.delay
+
+class MainActivity : AppCompatActivity() {
+  companion object {
+    private const val KEY_IMAGE_URI = "imageUri"
+    private const val IMAGE_PICK_REQUEST = 101
+  }
+
+  private var imageUri: Uri? = null
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    button.setOnClickListener {
+      val intent = Intent(Intent.ACTION_GET_CONTENT).also { it.type = "image/*" }
+      startActivityForResult(intent, IMAGE_PICK_REQUEST)
+      logd { "Pick a photo from media store." }
+    }
+
+    if (savedInstanceState != null && savedInstanceState.containsKey(KEY_IMAGE_URI)) {
+      logd { "Saved state contains an image Uri. Let's assign it to imageUri." }
+      imageUri = savedInstanceState.getParcelable(KEY_IMAGE_URI) as Uri
+    }
+  }
+
+  override fun onStart() {
+    super.onStart()
+    imageUri?.let {
+      logd { "We're starting up again with an existing imageUri. Let's load that." }
+      loadAndShowPhoto(it)
+    }
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    if (requestCode == IMAGE_PICK_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+      logd { "User picked photo with uri ${data.data}, let's load it" }
+      imageUri = data.data // So we can store it in onSaveInstanceState
+      loadAndShowPhoto(data.data)
+    }
+  }
+
+  override fun onSaveInstanceState(outState: Bundle?) {
+    super.onSaveInstanceState(outState)
+    if (outState != null && imageUri != null) {
+      logd { "Let's save the imageUri to our saved state so we can load it when we come back!" }
+      outState.putParcelable(KEY_IMAGE_URI, imageUri)
+    }
+  }
+
+  private fun loadAndShowPhoto(uri: Uri) {
+    load {
+      // This run on a background thread
+      logd { "Start loading image on thread ${Thread.currentThread().name}" }
+      delay(5000L) // Fake a long loading so we can test what happens in onStop()
+      MediaStore.Images.Media.getBitmap(contentResolver, uri)
+    } then {
+      // This runs on the main thread
+      logd { "Image with size ${it.width} x ${it.height} loaded. Display on ImageView running on thread ${Thread.currentThread().name}" }
+      imageView.setImageBitmap(it)
+    }
+  }
+}
